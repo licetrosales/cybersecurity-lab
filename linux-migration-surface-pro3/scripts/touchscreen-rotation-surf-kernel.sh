f476a393f41279b9f53d750034486efaa10ef720
Lis Ullmann <54541600+licetrosales@users.noreply.github.com>
2026-03-20 18:11:23 +0100

    refactor: use name of the device instead of id for the touchscreen

--- linux-migration-surface-pro3/scripts/touchscreen-rotation-surf-kernel.sh
+++ linux-migration-surface-pro3/scripts/touchscreen-rotation-surf-kernel.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
 DISPLAY="eDP-1"
-TOUCH_ID="12"
+TOUCH_NAME="NTRG0001:01 1B96:1B05"
+
+rotate_touch() {
+  case "$1" in
+    normal)
+      xinput set-prop "$TOUCH_NAME" "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1
+      ;;
+    inverted)
+      xinput set-prop "$TOUCH_NAME" "Coordinate Transformation Matrix" -1 0 1 0 -1 1 0 0 1
+      ;;
+    right)
+      xinput set-prop "$TOUCH_NAME" "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
+      ;;
+    left)
+      xinput set-prop "$TOUCH_NAME" "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1
+      ;;
+  esac
+}
 
 monitor-sensor | while read -r line; do
   case "$line" in
     *"normal"*)
       xrandr --output "$DISPLAY" --rotate normal
-      xinput set-prop "$TOUCH_ID" "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1
+      rotate_touch normal
       ;;
     *"bottom-up"*)
       xrandr --output "$DISPLAY" --rotate inverted
-      xinput set-prop "$TOUCH_ID" "Coordinate Transformation Matrix" -1 0 1 0 -1 1 0 0 1
+      rotate_touch inverted
       ;;
     *"right-up"*)
       xrandr --output "$DISPLAY" --rotate right
-      xinput set-prop "$TOUCH_ID" "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
+      rotate_touch right
       ;;
     *"left-up"*)
       xrandr --output "$DISPLAY" --rotate left
-      xinput set-prop "$TOUCH_ID" "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1
+      rotate_touch left
       ;;
   esac
 done
